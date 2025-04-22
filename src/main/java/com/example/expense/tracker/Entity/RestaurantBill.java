551251fb6ed0b9f112de787c2573814ff34dd787
ranjith.reddir <ranjith.velpur@gmail.com>
2025-04-22 01:26:38 -0500

    S123456:added github workflow floder

---
+++ src/main/java/com/example/expense/tracker/Entity/RestaurantBill.java
@@ -0,0 +1,22 @@
+package com.example.expense.tracker.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RestaurantBill {
+
+    @Id
+    private Int Bill_Id;
+
+    @
+
+
+
+}
